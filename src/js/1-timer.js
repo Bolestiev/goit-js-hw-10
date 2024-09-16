6304141c33d43e7bdcd3663ce38dcab443025c40
Anton Bolestiev <antonbolestiev1997@gmail.com>
2024-09-16 18:18:10 +0300

    new changes

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,84 +1,148 @@
+'use strict';
+
+// Описаний в документації
 import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+//import 'flatpickr/dist/themes/light.css';
+// Описаний у документації
 import iziToast from 'izitoast';
+// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const inputData = document.querySelector('.input-section input');
-const startButton = document.querySelector('.input-section button');
-const daysElement = document.querySelector('span[data-days]');
-const hoursElement = document.querySelector('span[data-hours]');
-const minutesElement = document.querySelector('span[data-minutes]');
-const secondsElement = document.querySelector('span[data-seconds]');
+let userSelectedDate = 0;
 
-let countdownInterval;
-startButton.disabled = true;
+//let userSelectedDateUnixTimestamp;
+// https://nesin.io/blog/javascript-date-to-unix-timestamp
+
+// https://www.scaler.com/topics/javascript-disable-button/
+const startButton = document.querySelector('button');
+//console.log(startButton.disabled);
+const inputField = document.querySelector('input');
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  //altInputClass: 'input',
   onClose(selectedDates) {
-    // Inside function of Flatpickr (event - change);
-    startButton.disabled = false;
-    const selectedDate = selectedDates[0];
-    // Press button - "Start" and start Timer
-    if (selectedDate.getTime() <= Date.now()) {
-      startButton.disabled = true;
-      iziToast.warning({
+    const dateNow = Date.now();
+    userSelectedDate = selectedDates[0];
+    //userSelectedDateUnixTimestamp = userSelectedDate.getTime();
+    //console.log(userSelectedDate.getTime());
+    //console.log(dateNow);
+    if (userSelectedDate.getTime() < dateNow) {
+      //window.alert('Please choose a date in the future');
+      iziToast.error({
+        title: 'Error',
         message: 'Please choose a date in the future',
-        position: 'topCenter',
-        color: 'red',
+        position: 'topRight',
+        titleColor: '#fff',
+        titleSize: '16px',
+        titleLineHeight: '1.5',
+        messageColor: '#fff',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
+        backgroundColor: '#ef4040',
+        theme: 'light',
+        iconUrl: 'izi-icon.svg', // зображення має бути у папці public
+        imageWidth: 24,
       });
+      startButton.disabled = true;
     } else {
-      startButton.addEventListener('click', startButtonClick);
-      function startButtonClick() {
-        iziToast.info({
-          message: 'Timer is started',
-          position: 'topCenter',
-        });
-        inputData.disabled = true;
-        startButton.disabled = true;
-        if (countdownInterval) {
-          clearInterval(countdownInterval); // Would clear previous interval if it added.
-        }
-        countdownInterval = setInterval(() => {
-          updateTimer(selectedDate);
-        }, 1000);
-        startButton.removeEventListener('click', startButtonClick);
-      }
+      startButton.disabled = false;
     }
   },
 };
 
-flatpickr(inputData, options);
+const fp = flatpickr('input#datetime-picker', options);
 
-function updateTimer(targetDate) {
-  const now = new Date().getTime();
-  const timeRemaining = targetDate.getTime() - now;
-  if (timeRemaining <= 0) {
-    clearInterval(countdownInterval);
-    daysElement.textContent = '00';
-    hoursElement.textContent = '00';
-    minutesElement.textContent = '00';
-    secondsElement.textContent = '00';
-    inputData.disabled = false;
-    iziToast.info({
-      message: 'Time is over!',
-      position: 'topCenter',
-    });
-    return;
-  }
+// Get the value of the "data-days" attribute
+//daysElement.innerText = '15';
 
-  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//console.log(document.querySelector('.timer .field .value[data-days]'));
+
+const interval = {
+  days: '03',
+  hours: '09',
+  minutes: '01',
+  seconds: '04',
+};
+
+function updateTimerInterface(interval) {
+  // Get the element with the "data-*" attributes
+  const daysElement = document.querySelector('.timer .field .value[data-days]');
+  const hoursElement = document.querySelector(
+    '.timer .field .value[data-hours]'
   );
-  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  const minutesElement = document.querySelector(
+    '.timer .field .value[data-minutes]'
+  );
+  const secondsElement = document.querySelector(
+    '.timer .field .value[data-seconds]'
+  );
 
-  daysElement.textContent = String(days).padStart(2, '0');
-  hoursElement.textContent = String(hours).padStart(2, '0');
-  minutesElement.textContent = String(minutes).padStart(2, '0');
-  secondsElement.textContent = String(seconds).padStart(2, '0');
+  // set interface elements
+  daysElement.innerText = interval.days;
+  hoursElement.innerText = interval.hours;
+  minutesElement.innerText = interval.minutes;
+  secondsElement.innerText = interval.seconds;
 }
+
+function convertUnixTimeToTime(milliseconds) {
+  // ToDo use padStart function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+  function addZero(s) {
+    if (s.length === 1) {
+      s = '0' + s;
+    }
+    return s;
+  }
+  // Отримання кількості днів, годин, хвилин та секунд
+  const days = Math.floor(milliseconds / (24 * 60 * 60 * 1000)).toString();
+  const hours = Math.floor(
+    (milliseconds % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+  ).toString();
+  const minutes = Math.floor(
+    (milliseconds % (60 * 60 * 1000)) / (60 * 1000)
+  ).toString();
+  const seconds = Math.floor((milliseconds % (60 * 1000)) / 1000).toString();
+
+  // Повернення результату у вигляді об'єкта
+  return {
+    days: addZero(days),
+    hours: addZero(hours),
+    minutes: addZero(minutes),
+    seconds: addZero(seconds),
+  };
+}
+
+startButton.addEventListener('click', () => {
+  //console.log('Button click handler');
+  startButton.disabled = true;
+  inputField.disabled = true;
+
+  // create interval
+  const intervalId = setInterval(() => {
+    const timeToGo = userSelectedDate - Date.now();
+    //console.log(timeToGo);
+    if (userSelectedDate - Date.now() > 0) {
+      updateTimerInterface(convertUnixTimeToTime(timeToGo));
+    } else {
+      startButton.disabled = false;
+      inputField.disabled = false;
+      clearInterval(intervalId);
+    }
+  }, 1000);
+});
+
+// Deactivate button
+startButton.disabled = true;
+inputField.disabled = false;
+//console.log(startButton.disabled);
+
+//test updateTimerInterface(days, hours, minutes, seconds) function
+//updateTimerInterface(interval);
+
+//console.log(Date.now());
+//console.log(convertUnixTimeToTime(1722538054379));
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,42 +1,112 @@
+'use strict';
+
 // Описаний у документації
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', createPromise);
+const iziCommon = {
+  message: 'Common message',
+  theme: 'dark',
+  position: 'topRight',
+  titleColor: '#fff',
+  titleSize: '16px',
+  titleLineHeight: '1.5',
+  messageColor: '#fff',
+  messageSize: '16px',
+  messageLineHeight: '1.5',
+  imageWidth: 24,
+};
 
-function createPromise(event) {
+const iziSuccess = {
+  ...iziCommon,
+  title: 'OK',
+  color: '#59a10d',
+  iconUrl: 'ok-icon.svg', // зображення має бути у папці public
+};
+
+const iziError = {
+  ...iziCommon,
+  title: 'Error',
+  color: '#ef4040',
+  iconUrl: 'error-icon.svg', // зображення має бути у папці public
+};
+
+const iziWarning = {
+  ...iziCommon,
+  title: 'Warning',
+  color: '#ffa000',
+  iconUrl: 'caution-icon.svg',
+};
+
+const button = document.querySelector('button');
+const delay = document.querySelector('input[name="delay"]');
+let promiceDelay = delay.value;
+
+delay.addEventListener('change', () => {
+  promiceDelay = delay.value;
+  //console.log(promiceDelay);
+});
+
+// ToDo: можна і без stateInputs. До інпутів можна добратись і через
+// stateForm.
+// Певно, через деякий час, я буду сам над собою сміятись. :)
+const stateInputs = document.querySelectorAll('input[name="state"]');
+const stateForm = document.querySelector('.form');
+//console.log(stateForm);
+
+const handleClick = event => {
   event.preventDefault();
-
-  const delay = event.target.elements.delay.value;
-
-  const state = event.target.elements.state.value;
-
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(`✅ Fulfilled promise in ${delay}ms`);
-      } else {
-        reject(`❌ Rejected promise in ${delay}ms`);
-      }
-    }, delay);
+  //console.log(stateForm);
+  let promiceState = '';
+  stateInputs.forEach(input => {
+    if (input.checked) {
+      promiceState = input.value;
+    }
   });
-  promise
-    .then(resolve => {
-      iziToast.success({
-        message: `${resolve}`,
-        messageSize: '20',
-        position: 'topCenter',
-        color: 'green',
-      });
-    })
-    .catch(error => {
-      iziToast.error({
-        message: `${error}`,
-        messageSize: '20',
-        position: 'topCenter',
-        color: 'red',
-      });
+  //console.log(promiceState, promiceDelay);
+  if (promiceState && promiceDelay) {
+    // console.log(`${promiceState} in delay: ${promiceDelay}`);
+
+    // Create promise
+    const promise = new Promise((resolve, reject) => {
+      //console.log('new Promice');
+      const savedPromiceDelay = promiceDelay;
+      setTimeout(() => {
+        if (promiceState === 'fulfilled') {
+          resolve(`✅ Fulfilled promise in ${savedPromiceDelay} ms`);
+        } else {
+          reject(`❌ Rejected promise in ${savedPromiceDelay} ms`);
+        }
+      }, promiceDelay);
     });
-}
+    // Registering promise callbacks
+    promise.then(
+      value => {
+        iziToast.success({
+          ...iziSuccess,
+          message: `${value.slice(1)}`,
+        });
+        console.log(value); // "Success! Value passed to resolve function"
+      },
+      error => {
+        iziToast.error({
+          ...iziError,
+          message: `${error.slice(1)}`,
+        });
+        console.log(error); // "Error! Error passed to reject function"
+      }
+    );
+    // reset form values
+    stateForm.reset();
+  } else {
+    iziToast.warning({
+      ...iziWarning,
+      message: 'Please fill in all fields.',
+    });
+    // window.alert('Please fill in all fields.');
+  }
+};
+
+//button.addEventListener('submit', handleClick);
+button.addEventListener('click', handleClick);
